114c39078b8d2158ae15a976338eb6761ba9a0cf
RedouaneAmourBaracoda <redouane.amour@baracoda.com>
2025-01-31 08:13:21 +0100

    Configure App Launch and change directories.

---
+++ NextRace/NextRace/Sources/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NextRace
+//
+//  Created by Redouane on 31/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to Next Race")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NextRace/NextRace/Sources/NextRaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextRaceApp.swift
+//  NextRace
+//
+//  Created by Redouane on 31/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct NextRaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
